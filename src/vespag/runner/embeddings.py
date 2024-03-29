3e14c3fb80fbd572a408ebb5330adfaff5aea8e8
C-Marquet <73125710+C-Marquet@users.noreply.github.com>
2024-03-29 22:24:09 +0100

    update embedding generation and inference

--- src/vespag/runner/embeddings.py
+++ src/vespag/runner/embeddings.py
@@ -1,12 +1,13 @@
-from transformers import AutoTokenizer, AutoModel
 import torch
-import numpy as np
 
 from pathlib import Path
 from typing import Optional
 
 from Bio import SeqIO
 import h5py
+import re
+
+from src.vespag.utils import get_device
 
 
 def get_esm2_embeddings(
@@ -15,33 +16,71 @@ def get_esm2_embeddings(
         model_name: str= "facebook/esm2_t36_3B_UR50D", 
         maxlen: Optional[int]= 4096,
     )-> None:
-    """    
-    Returns:
-    - numpy.ndarray: A matrix where each row is the embedding of a token in the text.
-    """
+    from transformers import AutoTokenizer, AutoModel
+
+    device = get_device()
 
     ids = [str(record.id) for record in SeqIO.parse(fasta_path, "fasta")]
     seqs = [str(record.seq) for record in SeqIO.parse(fasta_path, "fasta")]
-
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModel.from_pretrained(model_name)
-
-    inputs = tokenizer(seqs, return_tensors="pt", truncation=True, padding=True, max_length=maxlen) 
+    
+    tokenizer = AutoTokenizer.from_pretrained(model_name).to(device)
+    model = AutoModel.from_pretrained(model_name).to(device)
+    
+    inputs = tokenizer(seqs, return_tensors="pt", truncation=True, padding=True, max_length=maxlen).to(device) 
     with torch.no_grad():
         outputs = model(**inputs)
     
-    last_hidden_states = outputs.last_hidden_state[:, 1:-1, :].squeeze(0)
-    x = last_hidden_states.detach()
+    x = torch.tensor(outputs.last_hidden_state[:, 1:-1, :].squeeze(0), dtype=torch.float32)
+    # last_hidden_states = outputs.last_hidden_state[:, 1:-1, :].squeeze(0)
+    # x = last_hidden_states.detach()
 
-    with h5py.File(out_path, "w") as hdf:
+    with h5py.File(out_path + '/esm2_embeddings.h5', "w") as hdf:
         for label, embedding in zip(ids, x):
             hdf.create_dataset(name=label, data=embedding)
 
     return {label: emb for label, emb in zip(ids, x)}
 
+# TODO finish h5file creation (padding + length)
+# https://github.com/agemagician/ProtTrans/blob/master/Embedding/prott5_embedder.py
+def get_prott5_embeddings(
+        fasta_path: Path, 
+        out_path: Path,
+        model_name: str= "Rostlab/prot_t5_xl_uniref50", 
+        maxlen: Optional[int]= 4096,
+    )-> None:
+    from transformers import T5Tokenizer, T5EncoderModel
+
+    device = get_device()
+
+    ids = [str(record.id) for record in SeqIO.parse(fasta_path, "fasta")]
+    seqs = [str(record.seq) for record in SeqIO.parse(fasta_path, "fasta")]
+    # replace all rare/ambiguous amino acids by X and introduce white-space between all amino acids
+    seqs = [" ".join(list(re.sub(r"[UZOB]", "X", sequence))) for sequence in seqs]
+
+    tokenizer = T5Tokenizer.from_pretrained(model_name, do_lower_case=False).to(device)
+    model = T5EncoderModel.from_pretrained(model_name).to(device)
+
+    # only GPUs support half-precision currently; if you want to run on CPU use full-precision (not recommended, much slower)
+    model.full() if device=='cpu' else model.half()
+
+    # tokenize sequences and pad up to the longest sequence in the batch
+    seqs_tok = tokenizer.batch_encode_plus(seqs, add_special_tokens=True, padding="longest",return_tensors='pt').to(device)
+
+    # generate embeddings
+    with torch.no_grad():
+        embedding_rpr = model(
+                seqs_tok.input_ids, 
+                attention_mask=seqs_tok.attention_mask
+                )
+
+    # extract residue embeddings for the first ([0,:]) sequence in the batch and remove padded & special tokens, incl. prefix ([0,1:8]) 
+    emb_0 = embedding_rpr.last_hidden_state[0,1:8] # shape (7 x 1024)
+    # same for the second ([1,:]) sequence but taking into account different sequence lengths ([1,:6])
+    emb_1 = embedding_rpr.last_hidden_state[1,1:6] # shape (5 x 1024)
+
 
 if __name__ == '__main__':
     get_esm2_embeddings(
         fasta_path='/mnt/project/marquet/Variants/VespaG/VespaG/data/test/test.fasta', 
-        out_path='/mnt/project/marquet/Variants/VespaG/VespaG/data/test/test.h5',
+        out_path='/mnt/project/marquet/Variants/VespaG/VespaG/data/test',
         )
--- src/vespag/runner/predict.py
+++ src/vespag/runner/predict.py
@@ -5,6 +5,7 @@ from src.vespag.runner.type_hinting import *
 from src.vespag.runner.utils import *
 from src.vespag.utils import get_device
 from src.vespag.utils import load_model_from_config
+from src.vespag.runner.embeddings import get_esm2_embeddings
 
 
 import h5py
@@ -28,9 +29,9 @@ def load_model() -> torch.nn.Module:
 
 
 def predict(
-        embeddings_file: Path,
-        output: Path,
-        fasta_file: Optional[Path] = None,
+        out_path: Path,
+        fasta_file: Path,
+        embeddings_file: Optional[Path] = None,
         mutation_file: Optional[Path] = None,
         id_map: Optional[Path] = None,
         model: Architecture = "fnn",
@@ -39,26 +40,36 @@ def predict(
         no_csv: bool = False,
         h5_output: Optional[Path] = None,
         one_based_mutations: Optional[bool] = False,
-        idx: Optional[str] = '1idx',
+        zero_idx: Optional[bool] = False,
         # wandb_logdir: Path = Path("/mnt/project/schlensok/VESPA2/logs/vespa2")
 ) -> None:
-    if embeddings_file is None:
-        # TODO generate embeddings
-        pass
-
+    
     device = get_device()
-    # load embeddings
-    embeddings = {id: torch.tensor(emb[()], dtype=torch.float32, device=device) for id, emb in
-                  tqdm(h5py.File(embeddings_file).items(), desc="Loading embeddings")}
 
-    if id_map:
-        for line in id_map.open("r").readlines():
-            h5_id, fasta_id = line.split(",")
-            embeddings[fasta_id] = embeddings[h5_id]
-            del embeddings[h5_id]
+    # load VespaG
+    model = load_model()
+    model = model.to(device, dtype=torch.float32)
 
+    # compute embeddings
+    if embeddings_file is None:
+        embeddings = get_esm2_embeddings(fasta_file, out_path)
+    # or load precomputed embeddings
+    else:
+        # load embeddings
+        embeddings = {id: torch.tensor(emb[()], dtype=torch.float32, device=device) for id, emb in
+                    tqdm(h5py.File(embeddings_file).items(), desc="Loading embeddings")}
+    
+    # TODO check with JS if needed
+    # if id_map:
+    #     for line in id_map.open("r").readlines():
+    #         h5_id, fasta_id = line.split(",")
+    #         embeddings[fasta_id] = embeddings[h5_id]
+    #         del embeddings[h5_id]
+
+    # if mutation file provided, run VespaG for specific mutations
     if mutation_file:
         mutations_per_protein = read_mutation_file(mutation_file, one_indexed=one_based_mutations)
+    # no mutation file, run VespaG for entire mutational landscape
     elif fasta_file:
         # generate mutational landscape
         sequences = {record.id: str(record.seq) for record in SeqIO.parse(fasta_file, "fasta")}
@@ -69,58 +80,58 @@ def predict(
                 for other_aa in AMINO_ACIDS if other_aa != wildtype_aa]
             for protein_id, sequence in tqdm(sequences.items(), desc="Generating full mutational landscape")}
 
-    # TODO load best model for given architecture & embedding type
-    # if model in ["fnn", "cnn"]:
-    #     model = ModelLoader(wandb_logdir).load_best(model.value.lower(), embedding_type.value.lower())
-    # elif model == "mean":
-    #     fnn = ModelLoader(wandb_logdir).load_best("fnn", embedding_type.value.lower())
-    #     cnn = ModelLoader(wandb_logdir).load_best("cnn", embedding_type.value.lower())
-    #     model = MeanModel(fnn, cnn)
-
-    model = load_model()
-    model = model.to(device, dtype=torch.float32)
 
     # inference loop
     vespag_scores = {}
     scores_per_protein = {}
     for id, embedding in (pbar := tqdm(embeddings.items(), desc="Generating predictions")):
         pbar.set_postfix({"current_protein": id})
+        # get VespaG predictions
         y = model(embedding)
+        # set wildtype to 0
         if fasta_file:
             y = mask_non_mutations(y, sequences[id])
+            
+        # dict with mutation (fromAA,position,toAA) as key and score as item
+        if zero_idx:
+            scores_per_protein[id] = {
+                mutation: compute_mutation_score(y, mutation)
+                for mutation in mutations_per_protein[id]
+            }
+        else:
+            scores_per_protein[id] = {
+                f'{str(mutation)[0]}{int(str(mutation)[1])+1}{str(mutation)[2]}': compute_mutation_score(y, mutation)
+                for mutation in mutations_per_protein[id]
+            }
+        # optional: prepare h5 file
         if h5_output:
             vespag_scores[id] = y.detach()
-        # scores_per_protein[id] = {
-        #     mutation: compute_mutation_score(y, mutation)
-        #     for mutation in mutations_per_protein[id]
-        # }
-        scores_per_protein[id] = {
-            f'{str(mutation)[0]}{int(str(mutation)[1])+1}{str(mutation)[2]}': compute_mutation_score(y, mutation)
-            for mutation in mutations_per_protein[id]
-        }
 
-    # write output
-    if not no_csv:
-        if not single_csv:
-            # TODO verify that output is CSV file
-            with output.open("w+") as f:
-                f.writelines([line for line in tqdm([
-                    f"{protein_id}_{str(sav)},{score}\n"
-                    for protein_id, mutations in scores_per_protein.items()
-                    for sav, score in mutations.items()], desc="Generating output file")
-                              ]),
-        else:
-            # TODO verify that output is directory
-            for protein_id, mutations in tqdm(scores_per_protein.items(), desc="Generating output files"):
-                output_file = Path(output, protein_id + ".csv")
-                with output_file.open("w+") as f:
-                    f.writelines("Mutation, VespaG\n")
-                    f.writelines([f"{str(sav)},{score}\n" for sav, score in mutations.items()])
-
+    # write h5 file
     if h5_output:
         with h5py.File(h5_output, "w") as f:
             for id, vespag_prediction in tqdm(vespag_scores.items(), desc="Generating H5 output file"):
                 f.create_dataset(id, data=vespag_prediction)
+
+    # write csv output (default: single csv file for each mutational landscape of protein sequence)
+    if not no_csv:
+        if single_csv:
+            # TODO verify that out_path is directory
+            for protein_id, mutations in tqdm(scores_per_protein.items(), desc="Generating output files"):
+                output_file = Path(out_path, protein_id + ".csv")
+                with output_file.open("w+") as f:
+                    f.writelines("Mutation, VespaG\n")
+                    f.writelines([f"{str(sav)},{score}\n" for sav, score in mutations.items()])
+        else:
+            # TODO verify that out_path is CSV file
+            output_file = Path(out_path, "vespag_scores_all.csv")
+            with output_file.open("w+") as f:
+                f.writelines("Mutation, VespaG\n")
+                f.writelines([line for line in tqdm([
+                    f"{protein_id}_{str(sav)},{score}\n"
+                    for protein_id, mutations in scores_per_protein.items()
+                    for sav, score in mutations.items()], desc="Generating output file")
+                              ])
 
 
 if __name__ == '__main__':
