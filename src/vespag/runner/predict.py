2a13fd64a3f02c55dfbcb2b4f69b101b606d24fb
C-Marquet <73125710+C-Marquet@users.noreply.github.com>
2024-03-30 15:56:24 +0100

    add argparse to predict

--- src/vespag/runner/predict.py
+++ src/vespag/runner/predict.py
@@ -3,6 +3,7 @@ import h5py
 from tqdm import tqdm
 import torch
 from pathlib import Path
+import argparse
 from Bio import SeqIO
 
 from src.vespag.runner.type_hinting import *
@@ -10,6 +11,7 @@ from src.vespag.runner.utils import *
 from src.vespag.utils import get_device
 from src.vespag.utils import load_model_from_config
 from src.vespag.runner.embeddings import get_esm2_embeddings
+
 
 
 # def load_model(config_key: str, params: dict, checkpoint_dir: Path, embedding_type: str) -> torch.nn.Module:
@@ -33,9 +35,9 @@ def predict(
         id_map: Optional[Path] = None,
         model: Architecture = "fnn",
         embedding_type: EmbeddingType = "esm2",
-        single_csv: bool = True,
+        single_csv: bool = False,
         no_csv: bool = False,
-        h5_output: Optional[Path] = None,
+        h5_output: bool = False,
         one_based_mutations: Optional[bool] = False,
         zero_idx: Optional[bool] = False,
         # wandb_logdir: Path = Path("/mnt/project/schlensok/VESPA2/logs/vespa2")
@@ -106,7 +108,7 @@ def predict(
 
     # write h5 file
     if h5_output:
-        with h5py.File(h5_output, "w") as f:
+        with h5py.File(out_path + '/vespag_scores_all.h5', "w") as f:
             for id, vespag_prediction in tqdm(vespag_scores.items(), desc="Generating H5 output file"):
                 f.create_dataset(id, data=vespag_prediction)
 
@@ -131,15 +133,72 @@ def predict(
                               ])
 
 
+def create_arg_parser():
+    """"Creates and returns the ArgumentParser object."""
+    # Instantiate the parser
+    parser = argparse.ArgumentParser(description=( 
+            'predict.py generates VespaG variant effect score predictions'+
+            ' for a given file containing sequence(s) in FASTA-format.') )
+    # Required positional argument
+    parser.add_argument( '-i', '--input', required=True, type=str,
+                    help='A path to a fasta-formatted text file containing protein sequence(s).')
+    # Optional positional argument
+    parser.add_argument( '-o', '--output', required=False, type=str, 
+                    help='A path for saving the created CSV files. Default directory is "./data/output".')
+    # Optional positional argument
+    parser.add_argument( '-e', '--embeddings', required=False, type=str, 
+                    help='A path to pre-generated ESM-2 input embeddings. If not provided, embeddings will be saved in "./data/output/esm2_embeddings.h5"')
+    # Optional positional argument
+    parser.add_argument('--h5_output', required=False, type=str,
+                    default=None,
+                    help='Whether a file containing all predictions in HDF5 format should be saved.' )
+    # Optional positional argument
+    parser.add_argument('--single_csv', required=False, type=str,
+                    default=None,
+                    help='Whether to return one CSV file for all proteins instead of per-protein CSV files.' )
+    # Optional positional argument
+    parser.add_argument('--no_csv', required=False, type=str,
+                    default=None,
+                    help='Whether no CSV output should be produced.' )
+    # Optional argument
+    parser.add_argument('--zero_idx', type=bool, 
+                    default=False,
+                    help="Whether to enumerate the sequence starting at 0. Default is starting at 1.")
+    return parser
+
+def main():
+    parser     = create_arg_parser()
+    args       = parser.parse_args()
+    
+    #required
+    seq_path   = Path(args.input)
+    #optional
+    out_path   = Path(args.output) if args.output is not None else None
+    emb_path   = Path(args.embeddings) if args.embeddings is not None else None
+    h5_output = args.h5_output if args.h5_output is not None else None
+    no_csv = args.no_csv if args.zerono_csv_idx is not None else None
+    single_csv = args.single_csv if args.single_csv is not None else None
+    zero_idx = args.zero_idx if args.zero_idx is not None else None
+    
+    predict(fasta_file=seq_path, 
+            out_path=out_path,
+            embeddings_file=emb_path,
+            h5_output=h5_output,
+            no_csv=no_csv,
+            single_csv=single_csv, 
+            zero_idx=zero_idx,
+            )
+
 if __name__ == '__main__':
-    predict(embeddings_file= 'data/test/test.h5',
-        output= 'data/test/test',
-        fasta_file= 'data/test/test.fasta',
-        model = "fnn",
-        embedding_type = "esm2",
-        single_csv= True,
-        no_csv= False,
-        )
+    main()
+    # predict(embeddings_file= 'data/test/test.h5',
+    #     output= 'data/test/test',
+    #     fasta_file= 'data/test/test.fasta',
+    #     model = "fnn",
+    #     embedding_type = "esm2",
+    #     single_csv= True,
+    #     no_csv= False,
+    #     )
     # predict(embeddings_file= 'data/test/proteingym_217_esm2.h5',
     #     output= 'data/test/pg217_1idx',
     #     fasta_file= 'data/test/proteingym_217.fasta',
