5014c44a61b94452acb24e1def52f430d42724cd
Julius Schlensok <schlenju@gmail.com>
2025-02-06 13:36:39 +0100

    feat: add barebones testing & linting

---
+++ tests/integration_tests/test_workflows.py
@@ -0,0 +1,23 @@
+import polars as pl
+import polars.testing
+from typer.testing import CliRunner
+
+from vespag.__main__ import app
+
+runner = CliRunner()
+
+def test_predict(output_dir, fasta, score_files):
+    result = runner.invoke(app, [
+        "predict",
+        "-i", str(fasta),
+        "-o", str(output_dir)
+    ])
+
+    assert result.exit_code == 0
+
+    for score_file in score_files:
+        assert score_file.exists()
+
+        ground_truth_df = pl.read_csv(score_file)
+        generated_df = pl.read_csv(output_dir / score_file.name)
+        pl.testing.assert_frame_equal(generated_df, ground_truth_df)
