02f03190743743c06599c53bd3fdc14dcc1901f1
Julius Schlensok <schlenju@gmail.com>
2025-02-09 17:40:47 +0100

    test: add tests for score transformation & normalization

---
+++ tests/unit_tests/utils/test_utils.py
@@ -0,0 +1,29 @@
+import math
+import unittest
+
+import numpy as np
+from scipy import stats
+
+from vespag.utils.utils import ScoreNormalizer, transform_scores
+
+
+class TestUtils(unittest.TestCase):
+    def setUp(self):
+        # 100 randomly sampled mutations from ProteinGym217
+        self.raw_vespag_scores = np.loadtxt("tests/test_data/scores/raw_vespag_scores.csv", delimiter=",")
+        self.dms_scores = np.loadtxt("tests/test_data/scores/dms_scores.csv", delimiter=",")
+
+    def test_transform_scores(self) -> None:
+        """Test whether score transformation leads to a change in correlation with experimental scores."""
+        transformed_vespag_scores = transform_scores(self.raw_vespag_scores)
+        spearman_raw = stats.spearmanr(self.dms_scores, self.raw_vespag_scores).statistic
+        spearman_transformed = stats.spearmanr(self.dms_scores, transformed_vespag_scores).statistic
+        assert math.isclose(spearman_raw, spearman_transformed, abs_tol=0.001)
+
+    def test_normalize_scores(self) -> None:
+        """Test whether score normalization leads to a change in correlation with experimental scores."""
+        normalizer = ScoreNormalizer("sigmoid")
+        normalized_vespag_scores = normalizer.normalize_scores(self.raw_vespag_scores)
+        spearman_raw = stats.spearmanr(self.dms_scores, self.raw_vespag_scores).statistic
+        spearman_normalized = stats.spearmanr(self.dms_scores, normalized_vespag_scores).statistic
+        assert math.isclose(spearman_raw, spearman_normalized)
