31c838206a077e3f39f7569912504660573c05c0
Julius Schlensok <schlenju@gmail.com>
2025-02-25 21:19:21 +0100

    test: score transformation for prott5 model

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,3 +26,14 @@ def output_dir(tmp_path) -> Path:
     output_dir = tmp_path / "output"
     output_dir.mkdir()
     return output_dir
+
+@pytest.fixture(scope="module")
+def dms_vespag_scores() -> dict[str, ScoreList]:
+    return {
+        "esm2": np.loadtxt(test_data_dir / "dms_scores/raw_vespag_scores_esm2.csv", delimiter=","),
+        "prott5": np.loadtxt(test_data_dir / "dms_scores/raw_vespag_scores_prott5.csv", delimiter=",")
+    }
+
+@pytest.fixture(scope="module")
+def dms_experimental_scores() -> ScoreList:
+    return np.loadtxt(test_data_dir / "dms_scores/dms_scores.csv", delimiter=",")
--- tests/unit_tests/utils/test_utils.py
+++ tests/unit_tests/utils/test_utils.py
@@ -1,31 +1,25 @@
 import math
-import unittest
 
-import numpy as np
+import pytest
 from scipy import stats
 
 from vespag.utils.utils import ScoreNormalizer, transform_scores
 
 
-# TODO add tests for ProtT5-based scores
-class TestUtils(unittest.TestCase):
-    def setUp(self):
-        # 100 randomly sampled mutations from ProteinGym217
-        self.raw_vespag_scores_esm2 = np.loadtxt("tests/test_data/dms_scores/raw_vespag_scores_esm2.csv", delimiter=",")
-        self.dms_scores = np.loadtxt("tests/test_data/dms_scores/dms_scores.csv", delimiter=",")
+@pytest.mark.parametrize("embedding_type", ["esm2", "prott5"])
+def test_transform_scores(dms_vespag_scores, dms_experimental_scores, embedding_type) -> None:
+    """Test whether score transformation leads to a change in correlation with experimental scores."""
+    transformed_vespag_scores = transform_scores(dms_vespag_scores[embedding_type])
+    spearman_raw = stats.spearmanr(dms_experimental_scores, dms_vespag_scores[embedding_type]).statistic
+    spearman_transformed = stats.spearmanr(dms_experimental_scores, transformed_vespag_scores).statistic
+    assert math.isclose(spearman_raw, spearman_transformed, abs_tol=0.001)
 
-    def test_transform_scores_esm2(self) -> None:
-        """Test whether score transformation leads to a change in correlation with experimental scores."""
-        transformed_vespag_scores = transform_scores(self.raw_vespag_scores_esm2)
-        spearman_raw = stats.spearmanr(self.dms_scores, self.raw_vespag_scores_esm2).statistic
-        spearman_transformed = stats.spearmanr(self.dms_scores, transformed_vespag_scores).statistic
-        assert math.isclose(spearman_raw, spearman_transformed, abs_tol=0.001)
-
-    def test_normalize_scores_esm2(self) -> None:
-        """Test whether score normalization leads to a change in correlation with experimental scores."""
-        normalizer = ScoreNormalizer("minmax")
-        normalizer.fit(self.raw_vespag_scores_esm2)
-        normalized_vespag_scores = normalizer.normalize_scores(self.raw_vespag_scores_esm2)
-        spearman_raw = stats.spearmanr(self.dms_scores, self.raw_vespag_scores_esm2).statistic
-        spearman_normalized = stats.spearmanr(self.dms_scores, normalized_vespag_scores).statistic
-        assert math.isclose(spearman_raw, spearman_normalized)
+@pytest.mark.parametrize("embedding_type", ["esm2", "prott5"])
+def test_normalize_scores(dms_vespag_scores, dms_experimental_scores, embedding_type) -> None:
+    """Test whether score normalization leads to a change in correlation with experimental scores."""
+    normalizer = ScoreNormalizer("minmax")
+    normalizer.fit(dms_vespag_scores[embedding_type])
+    normalized_vespag_scores = normalizer.normalize_scores(dms_vespag_scores[embedding_type])
+    spearman_raw = stats.spearmanr(dms_experimental_scores, dms_vespag_scores[embedding_type]).statistic
+    spearman_normalized = stats.spearmanr(dms_experimental_scores, normalized_vespag_scores).statistic
+    assert math.isclose(spearman_raw, spearman_normalized)
