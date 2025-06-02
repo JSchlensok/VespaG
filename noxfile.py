d010b81af619e77523c47693d3879018010b2e4d
Julius Schlensok <34397338+JSchlensok@users.noreply.github.com>
2025-06-02 11:20:02 +0200

    Fix GitHub actions (#23)
    * feat: set logging level to INFO (#15)
    
    * Fix #14 (#16)
    
    * fix: compute prott5 embeddings if requested
    
    * test: add tests for prott5-based model
    
    * refactor: vectorize & simplify score transformation
    
    * style: remove obsolete import
    
    * feat: score transformation data for prott5 model
    
    * refactor: pure pytest tests instead of unittest
    
    * test: score transformation for prott5 model
    
    * build: delete build artifacts in GH actions container
    
    * fix: manually clean up space
    
    don't mess around with partitions
    
    add debug `df -h` after every significant step
    
    * fix(github actions): remove redundant runs
    
    don't run tests twice on every push to a PR
    
    * fix(github actions): remove debug steps
    
    * fix(github actions): use function-level scope for huggingface cache dir
    
    * fix(github actions): add missing import
    
    * build: clean up cache and output directories after each test run
    
    * fix(github actions): only create temporary embedding cache in test environment
    
    * build(github actions): don't run pytest in parallel mode in GH actions runner
    
    * build(github actions): fix yml syntax
    
    * build(github actions): remove more build artifacts
    
    * test: increase score comparison tolerance
    
    * build(github actions): make freed space available for default cache directory
    
    * fix(github actions): typo
    
    * build(github actions): move uv cache dir to freed-up space
    
    * build(github actions): move huggingface cache to freed-up space
    
    * build(github actions): turn parallel mode back on
    
    * build(github actions): simplify coverage execution
    
    * build(coverage): always combine coverage files
    
    * chore(deps): bump torch & transformers

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,5 @@
+import os
+
 import nox
 
 package = "vespag"
@@ -21,8 +23,7 @@ def tests(session: nox.Session) -> None:
     try:
         session.run("coverage", "run", "--parallel", "-m", "pytest")
     finally:
-        if session.interactive:
-            session.notify("coverage")
+        session.notify("coverage")
 
 
 @nox.session
@@ -30,7 +31,7 @@ def coverage(session: nox.Session) -> None:
     session.install("uv")
     session.run("uv", "pip", "install", "coverage[toml]")
     session.run("coverage", "combine")
-    session.run("coverage", "report", "-i")
+    session.run("coverage", "report")
 
 
 # TODO fix issues
